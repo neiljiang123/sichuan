6caaaa4753bf389319a38f7b0e734c677244fdf3
Lai Jiang <lj1312@nyu.edu>
2019-09-18 12:05:28 +0800

    my initial commit message

--- src/App.js
+++ src/App.js
@@ -5,20 +5,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>Sichuan</h1>
+     <img width="500" alt="a picture of Sichuan on the map of China" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRRb8vLteeAeaZqImo6Bg4LhNOoN13NfL0guB2awdHaW_rzwcxM"></img>
+     <img className="wow" width="500" alt="a picture of spicy Sichuanese hotpot"  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_UnRxWxrW9D0QAgvgNg6LAClG7DKmcVJ6d6MMvI-Kt06wGtMUcw"></img>
     </div>
   );
 }
